8037eafdb9db0bca78c243e87eba76bcec8fa67a
Joakim N <92833271+Echoinstrum@users.noreply.github.com>
2025-11-16 19:51:53 +0100

    first commit

---
+++ TestProjectUmbraco/ViewModels/CallbackFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MessagePack.Formatters;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestProjectUmbraco.ViewModels
+{
+    public class CallbackFormViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [Display(Name = "Name")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Email address")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,}))$", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "Phone is required")]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; } = null!;
+        [Required(ErrorMessage = "Please select an option")]
+        public string SelectedOption { get; set; } = null!;
+    }
+}
